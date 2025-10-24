ea5845f6231edaa928902b81bf160c8c916636d1
AKU <jonghunchae4@gmail.com>
2025-10-24 15:24:30 +0900

    script

---
+++ Assets/Script/Manager/SaveManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SaveManager
+{
+    static string path = Application.persistentDataPath + "/save.json";
+
+    public void Save(/*PlayerStat playerStat*/)
+    {
+        
+    }
+
+    public void Load(/*PlayerStat playerStat*/)
+    {
+        
+    }
+}
---
+++ Assets/Script/Player/PlayerStatData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStatData
+{
+    //PlayerStat 스크립트 완성 된 후 작업 예정
+    public Vector3 position;
+}
